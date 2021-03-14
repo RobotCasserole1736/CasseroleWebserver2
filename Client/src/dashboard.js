39beaa8a562cfbf5f90b3f469aa1d90534296486
Chris <chrisgerth010592@gmail.com>
2021-03-14 10:37:41 -0500

    cleanup, colored blinky svg icons added!

--- Client/src/dashboard.js
+++ Client/src/dashboard.js
@@ -1,4 +1,5 @@
 import { CircularGauge } from './circularGauge.js'
+import { Icon } from './icon.js'
 
 //////////////////////////////////////////////////
 // Logic to run on page load
@@ -7,6 +8,10 @@ import { CircularGauge } from './circularGauge.js'
 // Instantiate all widgets
 var widget1 = new CircularGauge('widget1', 'Test', 0,100,75,95);
 var widget2 = new CircularGauge('widget2', 'Shooter RPM', -100,0,-90,-10);
+var widget3 = new Icon('widget3', 'warning', "#FF0000", null, 'icons/warning.svg');
+var widget4 = new Icon('widget4', 'battery', "#FFFF00", "#555555", 'icons/battery.svg');
+var widget5 = new Icon('widget5', 'vision',  "#00FF00", null, 'icons/vision.svg');
+var widget6 = new Icon('widget6', 'power',   "#55AAFF", "#555555", 'icons/power.svg');
 
 
 // Start up rendering (never returns)
@@ -23,9 +28,37 @@ function mainRenderLoop(){
 
     widget1.setVal(testSin1); 
     widget2.setVal(-1.0 * testSin1);
+
+    if(testSin1 > 35.0){
+        widget3.setVal(Icon.kBLINK);
+    } else {
+        widget3.setVal(Icon.kOFF);
+    }
+
+    if(testSin1 < 50.0){
+        widget4.setVal(Icon.kON);
+    } else {
+        widget4.setVal(Icon.kOFF);
+    }
+
+    if(testSin1 > 50.0){
+        widget5.setVal(Icon.kON);
+    } else {
+        widget5.setVal(Icon.kOFF);
+    }
+
+    if(testSin1 > 43.0){
+        widget6.setVal(Icon.kBLINK);
+    } else {
+        widget6.setVal(Icon.kOFF);
+    }
     
     widget1.render();
     widget2.render();
+    widget3.render();
+    widget4.render();
+    widget5.render();
+    widget6.render();
 
     requestAnimationFrame(mainRenderLoop);
 }
---
+++ Client/src/icon.js
@@ -0,0 +1,84 @@
+
+export class Icon {
+
+    static kOFF = 0;
+    static kON  = 1;
+    static kBLINK  = 2;
+
+    static blinkPeriodLoops = 60;
+    static blinkDC = 0.6;
+
+    //////////////////////////////////////
+    // Public Class methods
+    //////////////////////////////////////
+    constructor(draw_div_id, title, color_on, color_off, icon) { 
+        this.draw_div_id = draw_div_id;
+        this.title = title;
+        this.color_on = color_on;
+        this.color_off = color_off;
+        this.icon = icon;
+        
+        // State Variable Defaults
+        this.hasData = false;
+        this.curState = 0;
+        this.blinkCounter = 0;
+
+        this.docElem = document.getElementById(draw_div_id);
+    }
+
+    // Call this when NT is disconnected, or data is otherwise not available
+    reportNoData(){
+        this.hasData = false;
+        this.curState = kOFF;
+    }
+
+    // Call this whenever a new state for the widget is available.
+    setVal(state) { 
+        this.curState = state;
+        this.hasData = true;
+    }
+
+    //Call once per render loop to redraw the gauge
+    render() {
+
+        // Pick the color based on the current state of the icon.
+        var iconDrawColor = this.color_off;
+  
+        if(this.curState == Icon.kBLINK){
+            //Pick on state for high enough counter values
+            if(this.blinkCounter > (Icon.blinkPeriodLoops * (1.0-Icon.blinkDC))){
+                iconDrawColor = this.color_on;
+            } 
+
+            //Reset counter at period
+            if(this.blinkCounter > Icon.blinkPeriodLoops){
+                this.blinkCounter = 0;
+            } else {
+                this.blinkCounter++;
+            }
+
+        } else {
+            //Non-blink - just use the state to drive the icon color
+            this.blinkCounter = 0;
+            if(this.curState == Icon.kON){
+                iconDrawColor = this.color_on;
+            }
+        }
+
+        if(iconDrawColor != null){
+            //Draw an icon with the specified color
+            this.docElem.innerHTML = "<div style=\"background-color:" + iconDrawColor + ";mask:url("+ this.icon +");width:100%;height:100%;position:relative\"></div>"
+        } else {
+            //Make the area blank
+            this.docElem.innerHTML = "";
+        }
+
+
+
+    }
+
+    //////////////////////////////////////
+    // Private, Helper methods
+    //////////////////////////////////////
+
+  }
