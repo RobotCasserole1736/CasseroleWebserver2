8dc752f62a6233b74bcb24338e359bea2d541b32
Chris <chrisgerth010592@gmail.com>
2021-03-16 17:13:01 -0500

    small WIP adding/removing stripcharts

--- Client/src/stripchart.js
+++ Client/src/stripchart.js
@@ -8,8 +8,20 @@ function addPlot(){
     numPlots++;
     var row = plotsTable.insertRow(-1);
     row.id = "plot"+numPlots.toString();
+    row.class = "outlined"
     var plotToAdd = new Plot(row.id);
     plotList[numPlots-1] = plotToAdd;
+}
+
+function removePlot(){
+    if(numPlots > 0){
+        var plotToRemove = numPlots; // assume last plot removed.
+        delete plotList[plotToRemove];
+        plotList[plotToRemove] = null;
+        plotsTable.deleteRow(plotToRemove-1);
+        numPlots--;
+    }
+    
 }
 
 window.handleStartBtnClick = handleStartBtnClick;
@@ -29,7 +41,7 @@ function handleAddChartBtnClick(){
 
 window.handleRmChartBtnClick = handleRmChartBtnClick;
 function handleRmChartBtnClick(){
-
+    removePlot();
 }
 
 window.handleZoomFullBtnClick = handleZoomFullBtnClick;
