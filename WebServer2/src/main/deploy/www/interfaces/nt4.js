924d74ae9bb75eb8fdd326eb6e74dd48c639d862
Chris Gerth <chrisgerth010592@gmail.com>
2021-05-13 23:27:46 -0500

    Even more NT4 WIP

--- WebServer2/src/main/deploy/www/interfaces/nt4.js
+++ WebServer2/src/main/deploy/www/interfaces/nt4.js
@@ -2,6 +2,76 @@ import msgpack from "./msgpack/msgpack";
 
 importScripts("msgpack/msgpack.js");
 
+export class NT4_Subscription{
+    prefixes = new Set();
+    options = NT4_SubscriptionOptions();
+    uid = -1;
+
+    toSubscribeObj(){
+        return {
+            "prefixes": this.prefixes,
+            "options": this.options.toObj(),
+            "subid": this.uid,
+        };
+    }
+
+    toUnSubscribeObj(){
+        return {
+            "subid": this.uid,
+        };
+    }
+}
+
+export class NT4_SubscriptionOptions {
+    immedeate = false;
+    logging = false;
+    periodicRate_s = 0.1;
+
+    toObj(){
+        return {
+            "immediate": this.immedeate,
+            "periodic": this.periodicRate_s,
+            "logging": this.logging,
+        };
+    }
+}
+
+export class NT4_Topic{
+    name = "";
+    type = "";
+    properties = NT4_TopicProperties();
+
+    toPublishObj(){
+        return {
+            "name": this.name,
+            "type": this.type,
+        }
+    }
+
+    toUnPublishObj(){
+        return {
+            "name": this.name,
+        }
+    }
+
+    toPropertiesObj(){
+        return {
+            "name": this.name,
+            "update": this.properties.toUpdateObj(),
+        }
+    }
+}
+
+export class NT4_TopicProperties{
+    isPersistant = false;
+
+    toUpdateObj(){
+        return {
+            "persistent": this.isPersistant,
+        }
+    }
+}
+
 export class NT4_Client {
 
 
@@ -18,57 +88,167 @@ export class NT4_Client {
         this.onConnect = onConnect_in;
         this.onDisconnect = onDisconnect_in;
 
-        this.topicValMap = new Map();
-        this.subscriptions = new Set();
+        this.subscriptions = new Map();
+        this.subscription_uid_counter = 0;
 
-        // WS Connection Defaults
+        this.topics = new Map();
+
+        // WS Connection State (with defaults)
         this.serverBaseAddr = serverAddr; 
         this.clientIdx = 0;
         this.useSecure = true;
         this.serverAddr = "";
-
-
+        this.serverConnectionActive = false;
+        this.serverTimeOffset_us = 0;
     }
 
-    announceTopic(name, defaultVal){
-        this.topicValMap.set(name, defaultVal);
-        this.onTopicAnnounce(name, defaultVal);
+    //////////////////////////////////////////////////////////////
+    // PUBLIC API
+
+    // Add a new subscription. Returns a subscription object
+    subscribeImmedeate(topicPatterns){
+        var newSub = NT4_Subscription();
+        newSub.uid = this.getNewSubUID();
+        newSub.options.immedeate = true;
+        newSub.options.periodicRate_s = 0;
+        newSub.prefixes = topicPatterns;
+        newSub.options.logging = false;
+
+        this.subscriptions.set(newSub.uid, newSub);
+        if(this.serverConnectionActive){
+            this.ws_subscribe(newSub);
+        }
+        return newSub;
     }
 
-    unAnnounceTopic(name){
-        this.topicValMap.delete(name);
-        this.onTopicUnAnnounce(name);
+    // Add a new subscription. Returns a subscription object
+    subscribePeriodic(topicPatterns, period){
+        var newSub = NT4_Subscription();
+        newSub.uid = this.getNewSubUID();
+        newSub.options.immedeate = false;
+        newSub.options.periodicRate_s = period;
+        newSub.prefixes = topicPatterns;
+        newSub.options.logging = false;
+
+        this.subscriptions.set(newSub.uid, newSub);
+        if(this.serverConnectionActive){
+            this.ws_subscribe(newSub);
+        }
+        return newSub;
     }
 
-    sendDataValue(name, timestamp, value){
-        this.topicValMap.set(name, value);
-        this.onNewTopicData(name, timestamp, value);
+    // Add a new subscription. Returns a subscription object
+    subscribeLogging(topicPatterns){
+        var newSub = NT4_Subscription();
+        newSub.uid = this.getNewSubUID();
+        newSub.options.immedeate = false;
+        newSub.options.periodicRate_s = 0;
+        newSub.prefixes = topicPatterns;
+        newSub.options.logging = true;
+
+        this.subscriptions.set(newSub.uid, newSub);
+        if(this.serverConnectionActive){
+            this.ws_subscribe(newSub);
+        }
+        return newSub;
     }
 
-    subscribe(topicPattern){
-        this.subscriptions.add(topicPattern);
-    }
-
-    unSubscribe(topicPattern){
-        this.subscriptions.delete(topicPattern);
-    }
-
-    clearAllSubscriptions(){
-        this.subscriptions.clear();
-    }
-
-    getMostRecentValue(name){
-        if(this.testTopicsMap.has(name)){
-            return this.testTopicsMap.get(name);
-        } else {
-            return null;
+    // Given an existing subscription, unsubscribe from it.
+    unSubscribe(sub){
+        this.subscriptions.delete(sub.uid);
+        if(this.serverConnectionActive){
+            this.ws_unsubscribe(sub);
         }
     }
 
-    //Gets a timestamp in the NT4 time scaling & domain
+    // Unsubscribe from all current subscriptions
+    clearAllSubscriptions(){
+        for(var sub in this.subscriptions.values()){
+            this.unSubscribe(sub);
+        }
+    }
+
+    // Gets a timestamp in the NT4 time scaling & domain
     // I have no idea ift his will be useful going forward but..
-    getCurTimestamp(){
-        return new Date().getTime();
+    getCurTimestamp_us(){
+        return new Date().getTime()*1000 + this.serverTimeOffset_us;
+    }
+
+    
+    // Set the properties of a particular topic
+    setProperties(topic, isPersistant){
+        topic.properties.isPersistant = isPersistant;
+        if(this.serverConnectionActive){
+            this.ws_setproperties(topic);
+        }
+    }
+
+    // Publish a new topic from this client with the provided name and type
+    publishNewTopic(name, type){
+        var newTopic = NT4_Topic()
+        newTopic.name = name;
+        newTopic.type = type;
+
+        this.topics.set(newTopic.name, newTopic);
+        if(this.serverConnectionActive){
+            this.ws_publish(newTopic);
+        }
+
+        return newTopic;
+    }
+
+    // UnPublish a previously-published topic from this client.
+    unPublishTopic(oldTopic){
+        this.topics.delete(oldTopic.name);
+        if(this.serverConnectionActive){
+            this.ws_unpublish(oldTopic);
+        }
+    }
+
+    // Send some new value to the server
+    // Timestamp is whatever the current time is.
+    addSample(topic, value){
+        var timestamp = this.getCurTimestamp_us();
+        this.addSample(topic, timestamp, value);
+    }
+
+    // Send some new timestamped value to the server
+    addSample(topic, timestamp, value){
+        //TODO - send msgpack message? or something like that
+    }
+
+    //////////////////////////////////////////////////////////////
+    // Websocket Message Send Handlers
+
+    ws_subscribe(sub){
+        this.ws_sendJSON("subscribe", sub.toSubscribeObj());
+    }
+
+    ws_unsubscribe(sub){
+        this.ws_sendJSON("unsubscribe", sub.toUnSubscribeObj());
+    }
+
+    ws_publish(topic){
+        this.ws_sendJSON("publish", topic.toPublishObj());
+    }
+
+    ws_unpublish(topic){
+        this.ws_sendJSON("unpublish", topic.toUnPublishObj());
+    }
+
+    ws_setproperties(topic){
+        this.ws_sendJSON("setproperties", topic.toPropertiesObj());
+    }
+
+    ws_sendJSON(method, params){
+        if(this.ws.readyState == WebSocket.OPEN){
+            var txObj = {
+                "method": method,
+                "params": params
+            }
+            var txJSON = JSON.stringify(txObj);
+            this.ws.send(txJSON);
+        }
     }
 
     //////////////////////////////////////////////////////////////
@@ -76,10 +256,31 @@ export class NT4_Client {
 
     ws_onOpen() {
 
+        //Publish any existing topics
+        for(var topic in this.topics.values()){
+            this.ws_publish(topic);
+            this.ws_setproperties(topic);
+        }
+
+        //Subscribe to existing subscriptions
+        for(var sub in this.subscriptions.values()){
+            this.ws_subscribe(sub);
+        }
+
+        // Set the flag allowing general server communication
+        this.serverConnectionActive = true;
+
+        // User connection-opened hook
+        this.onConnect();
     }
 
     ws_onClose(e) {
+        //Clear flags to stop server communication
         this.ws = null;
+        this.serverConnectionActive = false;
+
+        // User connection-closed hook
+        this.onDisconnect();
 
         console.log('Socket is closed. Reconnect will be attempted in 0.5 second.', e.reason);
         setTimeout(this.ws_connect.bind(this), 500);
@@ -95,32 +296,44 @@ export class NT4_Client {
         if(typeof e.data === 'string'){
             //JSON Message
             var msg = JSON.parse(e.data); 
-            if(typeof msg === 'object'){
-                if( !("method" in msg) || !("params" in msg)){
-                    var method = msg["method"];
-                    var params = msg["params"];
-                    if(typeof method === 'string'){
-                        if(typeof params === 'object'){
-                            if(method == "announce"){
 
-                            } else if (method == "unannounce"){
-
-                            } else {
-                                console.log("Ignoring text message - unknown method " + method);
-                            }
-
-                        } else {
-                            console.log("Ignoring text message, JSON parsing found \"params\", but it wasn't an object.");
-                        }
-                    } else {
-                        console.log("Ignoring text message, JSON parsing found \"method\", but it wasn't a string.");
-                    }
-                } else {
-                    console.log("Ignoring text message, JSON parsing did not find all required fields."); 
-                }
-            } else {
+            //Validate proper format of message
+            if(typeof msg !== 'object'){
                 console.log("Ignoring text message, JSON parsing did not produce an object.");
+                return;
             }
+                
+            if( !("method" in msg) || !("params" in msg)){
+                console.log("Ignoring text message, JSON parsing did not find all required fields."); 
+                return;
+            }
+
+            var method = msg["method"];
+            var params = msg["params"];
+            
+            if(typeof method !== 'string'){
+                console.log("Ignoring text message, JSON parsing found \"method\", but it wasn't a string.");
+                return;
+            }
+                        
+            if(typeof params === 'object'){
+                console.log("Ignoring text message, JSON parsing found \"params\", but it wasn't an object.");
+                return;
+            }
+
+            // Message validates reasonably, switch based on supported methods
+            if(method == "announce"){
+                var newTopic = NT4_Topic();
+                this.onTopicAnnounce(newTopic);
+
+            } else if (method == "unannounce"){
+                this.onTopicUnAnnounce(removedTopic);
+
+            } else {
+                console.log("Ignoring text message - unknown method " + method);
+                return;
+            }
+
 
         } else {
             //MSGPack
@@ -137,7 +350,7 @@ export class NT4_Client {
         var prefix = "ws://";
         if(this.useSecure){
             prefix = "wss://"; //Use secure if requested.
-            port = 5810;
+            port = 5811;
         }
 
         this.serverAddr = prefix + this.serverBaseAddr + ":" + port.toString() + "/ns/" + "CasseroleWS2_" + this.clientIdx.toString();
@@ -152,6 +365,13 @@ export class NT4_Client {
     
 
 
+    //////////////////////////////////////////////////////////////
+    // General utilties
+    
+    getNewSubUID(){
+        this.subscription_uid_counter++;
+        return this.subscription_uid_counter;
+    }
 
 
 }
